e30af9053e9e30c22948fba451cb7165584bf3ae
David Rhoads <kd2flz@outlook.com>
2020-04-21 19:23:29 -0400

    added Brave to installer

--- install_awesome.sh
+++ install_awesome.sh
@@ -3,7 +3,17 @@
 sudo apt install -y awesome fonts-roboto rofi compton i3lock xclip gnome-keyring materia-gtk-theme papirus-icon-theme lxappearance xbacklight kde-spectacle guake keepassxc flatpak &&
 
 sudo snap install telegram-desktop
-sudo snap install chromium
+
+#Install Brave Browser
+sudo apt install apt-transport-https curl
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update &&
+sudo apt install brave-browser
+
+#Comment out brave and uncomment chromium below if you don't want to use Brave for some reason (your really should, though :)
+#Note that you must change the default application in apps.lua for this to work properly
+#sudo snap install chromium
 
 #Clone the repository (This is my fork, using awesome 4.3. If it doesn't work, comment it out and uncomment the awesome 4.2 version)
 git clone https://github.com/kd2flz/material-awesome.git ~/.config/awesome
