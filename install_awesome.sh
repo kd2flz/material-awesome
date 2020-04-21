e39e59a019f1440bfeb226f656076448c5d171c4
David Rhoads <kd2flz@outlook.com>
2020-04-21 17:02:34 -0400

    updated install file

--- install_awesome.sh
+++ install_awesome.sh
@@ -5,8 +5,8 @@ sudo apt install -y awesome fonts-roboto rofi compton i3lock xclip gnome-keyring
 sudo snap install telegram-desktop
 sudo snap install chromium
 
-#Clone the repository (This is Chris Titus's fork, using awesome 4.3. If it doesn't work, comment it out and uncomment the awesome 4.2 version)
-git clone https://github.com/ChrisTitusTech/material-awesome.git ~/.config/awesome
+#Clone the repository (This is my fork, using awesome 4.3. If it doesn't work, comment it out and uncomment the awesome 4.2 version)
+git clone https://github.com/kd2flz/material-awesome.git ~/.config/awesome
 #Uncomment the following line to use Awesome 4.2
 #git clone -b awesome-4.2 https://github.com/PapyElGringo/material-awesome.git ~/.config/awesome
 
